9510b1e6af0942adf9a9f6df18e0e897665ec64d
Istvan Szalai <istvan@diffagency.com>
2019-06-08 15:15:31 -0400

    v2

--- test.py
+++ test.py
@@ -7,30 +7,29 @@ OPTIONS = "sout=#transcode{vcodec=none,acodec=mp3,ab=128,channels=2,samplerate=4
 instance = vlc.Instance()
 
 #Create a MediaPlayer with the default instance
-player = instance.media_player_new()
-
-def set_track(player,media):
-  player.set_media(media)
-  player.play()
-
+player = instance.media_list_player_new()
 
 #Load the media file
+
 forest = instance.media_new('./data/forest.mp3', OPTIONS)
 god = instance.media_new('./data/god.mp3', OPTIONS)
 kids = instance.media_new('./data/kids.mp3', OPTIONS)
 water = instance.media_new('./data/water_wheel.mp3', OPTIONS)
 
-set_track(player,forest)
+tracks = [forest, kids, water, god]
+media_list = instance.media_list_new()
+
+media_list.lock()
+index = 0
+for track in tracks:
+  media_list.insert_media(track,index)
+  index+=1
+media_list.unlock()
+
+player.set_media_list(media_list)
+player.play()
 
 while True:
-  track = input("Select an audio track: 1:kids, 2:god, 3:water, 4:forest\n")
-  if track == '1':
-    set_track(player,kids)
-  elif track == '2':
-    set_track(player,god)
-  elif track == '3':
-    set_track(player,water)
-  elif track == '4':
-    set_track(player,forest)
-  else:
-    print('Wrong input\n')
+  track = input("Press a key to play next track\n")
+  if track != None:
+    player.next()
